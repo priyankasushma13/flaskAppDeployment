5011c1d4dbc915ddbb65078288538bfb4feec239
Sushma Priyanka <78401534+priyankasushma13@users.noreply.github.com>
2024-05-10 13:18:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    return "<h2> Hello WOrld - Docker Flask Deployment Lab </h2> <hr/>"
+    
+    return "<h2> Hello I am Sushma - Docker Flask Deployment Lab </h2> <hr/>"
 
 app.run(host="0.0.0.0" , port = 5000)
